9fd2de5638367e5857702fd223c59dfdc0624376
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 19:09:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title*, books.year* FROM books
+  "SELECT (books.title*), (books.year*) FROM books
   ORDER BY books.year WHERE
   books.series_id = series.id; "
 end
