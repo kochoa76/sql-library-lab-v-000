529f55ca7a61d91cc06990d253a73f420a5f65b7
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 20:33:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,6 +36,6 @@ def select_character_names_and_number_of_books_they_are_in
   ON character_books.character_id = characters.id
   GROUP BY characters.name
   ORDER BY COUNT(*) DESC
-  LIMIT 1;
+  ;
   "
 end
