86f27ca4f79f53189656ef51b61ceb3d7f08ca31
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 19:25:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,9 +9,9 @@ def select_name_and_motto_of_char_with_longest_motto
   ORDER BY LENGTH(characters.motto) DESC LIMIT 1; "
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) FROM characters 
+  WHERE characters.species = "human";"
 end
 
 def select_name_and_series_subgenres_of_authors
