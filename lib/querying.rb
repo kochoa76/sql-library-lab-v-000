09ac8423796f9fb76d85863174e6188d02c36771
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 19:17:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-  HAVING MAX(characters.motto); "
+  GROUP BY characters.name HAVING MAX(characters.motto); "
 end
 
 
