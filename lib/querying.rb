369ba34f0f66111e84bcc4b4f2b039c4670a924c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 19:56:32 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
   JOIN subgenres ON subgenres.id = series.subgenre_id 
-  JOIN series ON series.authors_id = authors.id;"
+  JOIN series ON series.author_id = authors.id;"
 end
 
 def select_series_title_with_most_human_characters
