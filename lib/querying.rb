57c36c8e19633219d75be959f3a16372a3fe0d31
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 19:58:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series  
+  WHERE characters.species = 'human'
+  ORDER BY SUM(characters.species) DESC LIMIT 1; "
 end
 
 def select_character_names_and_number_of_books_they_are_in
