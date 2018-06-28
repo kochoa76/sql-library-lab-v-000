0205160ca1c79d66918ec26d54388f2f74a5c4d6
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 20:06:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,8 +23,9 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
+  JOIN series ON series.id = characters.series_id 
   WHERE characters.species = 'human'
-  ORDER BY SUM(characters.species) DESC LIMIT 1; "
+  ORDER BY COUNT(characters.species) DESC LIMIT 1; "
 end
 
 def select_character_names_and_number_of_books_they_are_in
